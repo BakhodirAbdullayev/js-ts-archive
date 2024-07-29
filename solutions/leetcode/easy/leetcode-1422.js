0a2e21938e98769ede119330a61390b5a4f129c0
Bakhodir Abdullaev <bahodirabdullayev0556@gmail.com>
2024-07-29 21:11:48 +0500

    leetcode 1422 mistake removed

--- solutions/leetcode/easy/leetcode-1422.js
+++ solutions/leetcode/easy/leetcode-1422.js
@@ -4,14 +4,16 @@
  * @param {string} s
  * @return {number}
  */
+
 var maxScore = function (s) {
-  let max = -1;
   let len = s.length;
-  console.log("jel");
+  if (len === 1) {
+    return 1;
+  }
+  let max = -1;
   for (let i = 1; i < len; i++) {
     let s1 = sum(s.slice(0, i), "0"),
       s2 = sum(s.slice(i), "1");
-    console.log(s1, s2);
     if (max <= s1 + s2) {
       max = s1 + s2;
     }
@@ -22,11 +24,9 @@ var maxScore = function (s) {
 function sum(str, num) {
   let sum = 0;
   for (let i of str) {
-    if (i == num) {
+    if (i === num) {
       sum++;
     }
   }
   return sum;
 }
-
-console.log(maxScore("00"));
